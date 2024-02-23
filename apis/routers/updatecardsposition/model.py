819f2a3a9ab04975d71713239759aaa183aaab88
dbvt19 <93956271+dbvt19@users.noreply.github.com>
2024-02-23 18:39:59 +0900

    ブラッシュアップ

--- apis/routers/updatecardsposition/model.py
+++ apis/routers/updatecardsposition/model.py
@@ -23,15 +23,4 @@ class Model(BaseModel):
             # テーブル更新
             await database.execute(query)
 
-        # dt = Data(
-        #     card_id=body.card_id,
-        #     card_pos=body.card_pos,
-        #     col_id=body.col_id,
-        #     # uid=token.uid,
-        #     card_name=body.card_name,
-        #     input_date=body.input_date,
-        #     due_date=body.due_date,
-        #     color=body.color,
-        #     description=body.description
-        # )
         return Response(status=1, message="カードの位置情報(card_posおよびcol_id)を更新しました")
