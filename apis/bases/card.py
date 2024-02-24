89243ff4bf1aaf2b6aef079c94f02e783c38475d
dbvt19 <93956271+dbvt19@users.noreply.github.com>
2024-02-24 23:15:20 +0900

    delcategoryAPIの追加(カテゴリ内にカードがある場合は削除できない)

--- apis/bases/card.py
+++ apis/bases/card.py
@@ -9,7 +9,7 @@ class Card(Base):
     __table_args__ = {"extend_existing": True}  # 既存テーブルの再定義を認める
     card_id = Column(String(36), primary_key=True)
     card_pos = Column(Integer(unsigned=True))
-    col_id = Column(String(36), ForeignKey(Category.col_id) ,nullable=False)
+    col_id = Column(String(36), ForeignKey(Category.col_id) , nullable=False)
     uid = Column(String(36), ForeignKey(User.uid), nullable=False)
     card_name = Column(String(255), nullable=False, index=True)
     input_date = Column(Date, nullable=True)
